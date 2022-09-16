8b12451b2b80e43cd05e316431a014b782f5f7c7
kienrang <kienhtph17743@fpt.edu.vn>
2022-09-16 23:39:14 +0700

    project-front-end Kinglap

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <h1 className='text-danger'>Front-end Kinglap</h1>
+  );
+}
+
+export default App;
